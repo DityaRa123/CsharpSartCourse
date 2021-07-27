bd814bd1a4fe9eb1331f87d54eb730e8235c3bc1
KriS <KriS@DESKTOP-4RET6R8>
2021-07-27 21:07:16 +0300

    Добавьте файлы проекта.

---
+++ CsharpSartCourse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CsharpSartCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя");
+            var name = Console.ReadLine();
+            Console.WriteLine("Привет," + name + "!");
+        }
+    }
+}
